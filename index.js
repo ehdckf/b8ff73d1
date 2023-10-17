fccc6eda9543d895b5814aaeccd09be118424930
ehdckf <ehdcjf@funnyfig.com>
2023-10-17 18:01:15 +0900

    🤨

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
-const params = `hash=681c32ea7733fcd01135e5ad48a0a8db&providerId=providerLogin_1&userId=1&roundId=123451&smResult=1%3A10%3B2%3B9%3B3%3B2%233%3B1%3B2%3B1%3B4%2312%3B12%3B12%3B4%3B1%23R%23S%23VS%23222%23MV%232%2C00%23MT%232%23&betMultiplier=50`;
+const crypto = require("crypto");
 
-const x = new URLSearchParams(params);
-console.log(x);
+const response = {
+        a: "hello",
+        b: "world",
+        z: "hell",
+        v: "o",
+        h: "world",
+};
 
-console.log(x.keys());
+const secret = "funnyfig";
+
+function calcHash(params) {
+        const paramString = Object.entries(params)
+                .sort((a, b) => a[0].localeCompare(b[0]))
+                .map((v) => v.join("="))
+                .join("&");
+
+        const plainText = paramString + secret;
+        const hash = crypto.createHash("md5").update(plainText).digest("hex");
+        return hash;
+}
+
+function encodeParams(response) {
+        const paramString = Object.entries(response)
+                .sort((a, b) => a[0].localeCompare(b[0]))
+                .map((v) => v.join("="))
+                .join("&");
+
+        const plainText = paramString + secret;
+        const hash = crypto.createHash("md5").update(plainText).digest("hex");
+        return `hash=${hash}&${paramString}`;
+}
+
+function decodePragmaticRequest(text) {
+        let hash;
+        const params = {};
+        text.split("&").forEach((v) => {
+                const [key, value] = v.split("=");
+                if (key == "hash") {
+                        hash = value;
+                } else {
+                        params[key] = value;
+                }
+        });
+        if (hash == calcHash(params)) {
+                return true;
+        }
+        return false;
+}
+
+const result = encodeParams(response);
+console.log(result);
+console.log();
+console.log(decodePragmaticRequest(result));
